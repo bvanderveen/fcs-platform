e76be040839429cc5152363455a84444e788bc8c
Benjamin van der Veen <b@bvanderveen.com>
2014-10-19 01:07:10 -0700

    run hardware tests before program

--- platform/src/build-platform.sh
+++ platform/src/build-platform.sh
@@ -7,4 +7,7 @@ cd build
 cmake -D CMAKE_TOOLCHAIN_FILE=/opt/rpi/pi.cmake ../
 make
 scp -oStrictHostKeyChecking=no platform/fcs-platform $host:/home/fcs-platform
+scp -oStrictHostKeyChecking=no chr-um6/tests/um6_hardware_test $host:/home/um6_hardware_test
+ssh -t $host /home/um6_hardware_test
 ssh -t $host /home/fcs-platform
+
