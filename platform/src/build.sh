dd42a11553ba3fd5b5580539f43d4c1b848eda25
Benjamin van der Veen <b@bvanderveen.com>
2014-12-11 21:16:33 -0800

    read host in deploy script, fix binary path

--- platform/src/build.sh
+++ platform/src/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-host="$(cat /opt/rpi/host)"
 mkdir build
 cd build
 cmake -D CMAKE_TOOLCHAIN_FILE=/opt/rpi/pi.cmake ../
--- platform/src/deploy.sh
+++ platform/src/deploy.sh
@@ -1,5 +1,8 @@
 
-#scp -oStrictHostKeyChecking=no chr-um6/tests/um6_hardware_test $host:/home/um6_hardware_test
+host="$(cat /opt/rpi/host)"
+
+#scp -oStrictHostKeyChecking=no build/chr-um6/tests/um6_hardware_test $host:/home/um6_hardware_test
 #ssh -t $host /home/um6_hardware_test
-scp -oStrictHostKeyChecking=no platform/fcs-platform $host:/home/fcs-platform
+
+scp -oStrictHostKeyChecking=no build/platform/fcs-platform $host:/home/fcs-platform
 ssh -t $host /home/fcs-platform
