031b8139882c602adccd02ffb48f82206321a17d
Benjamin van der Veen <b@bvanderveen.com>
2015-01-05 01:12:29 -0800

    compiler container now just runs the build script, expects source and build output directories to be linked into container

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 set -e
 
 docker build -t dev platform
-docker run -t dev
+docker run --rm -v $(pwd)/platform/src:/opt/src -v $(pwd)/build:/opt/build -t dev
--- platform/src/build.sh
+++ platform/src/build.sh
@@ -3,7 +3,7 @@ set -e
 
 python creghead.py gp9_regs.py > platform/gp9_regs.h
 
-mkdir build
-cd build
-cmake -D CMAKE_TOOLCHAIN_FILE=/opt/rpi/pi.cmake ../
+cd /opt/build
+cmake -D CMAKE_TOOLCHAIN_FILE=/opt/rpi/pi.cmake /opt/src
 make
+
