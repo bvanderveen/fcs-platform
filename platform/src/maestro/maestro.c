03a2ecf77b6d917f5d1490e3c9b9ca3ec51cca5b
Benjamin van der Veen <b@bvanderveen.com>
2015-01-05 01:18:44 -0800

    place bounds on servo position, report errors. delete a bunch of cruft.

--- platform/src/maestro/maestro.c
+++ platform/src/maestro/maestro.c
@@ -28,7 +28,13 @@ void servo_controller_dealloc(servo_controller *controller) {
     }
 }
 
-void servo_controller_set_position(servo_controller *controller, uint8_t channel, float value) {
+int servo_controller_set_position(servo_controller *controller, uint8_t channel, float value) {
+    if (value < 0) {
+        value = 0;
+    }
+    if (value > 1.0) {
+        value = 1.0;
+    }
     float scaled = (value * 4000) + 4000;
     int target = (int)roundf(scaled);
     printf("[servo_controller] Setting ch%d to %f (%d us)\n", channel, value, target/4);
@@ -37,85 +43,7 @@ void servo_controller_set_position(servo_controller *controller, uint8_t channel
     if (write(controller->fd, command, sizeof(command)) == -1)
     {
         perror("[servo_controller] error writing");
-        //return -1;
+        return -1;
     }
-    //return 0;
-}
-
-
-
-// Gets the position of a Maestro channel.
-// See the "Serial Servo Commands" section of the user's guide.
-int maestroGetPosition(int fd, unsigned char channel)
-{
-  unsigned char command[] = {0x90, channel};
-  if(write(fd, command, sizeof(command)) == -1)
-  {
-    perror("error writing");
-    return -1;
-  }
-   
-  unsigned char response[2];
-  if(read(fd,response,2) != 2)
-  {
-    perror("error reading");
-    return -1;
-  }
-   
-  return response[0] + 256*response[1];
-}
- 
-// Sets the target of a Maestro channel.
-// See the "Serial Servo Commands" section of the user's guide.
-// The units of 'target' are quarter-microseconds.
-int maestroSetTarget(int fd, unsigned char channel, unsigned short target)
-{
-  unsigned char command[] = {0x84, channel, target & 0x7F, target >> 7 & 0x7F};
-  if (write(fd, command, sizeof(command)) == -1)
-  {
-    perror("error writing");
-    return -1;
-  }
-  return 0;
-}
-
-void setServoPosition(int fd, unsigned char channel, float position) 
-{
-    float scaled = (position * 4000) + 4000;
-    int target = (int)roundf(scaled);
-    printf("Setting ch%d to %f (%d us)\n", channel, position, target/4);
-    maestroSetTarget(fd, 0, target);
-}
-
-float randomFloat() {
-    float r = (float)rand();
-    float normalized = r / (float)RAND_MAX;
-    return normalized;
-}
-
-void talkToServos()
-{
-  printf("Let's talk to servos!\n");
-
-  // Open the Maestro's virtual COM port.
-  const char * device = "/dev/ttyACM0";  // Linux
-  int fd = open(device, O_RDWR | O_NOCTTY);
-  if (fd == -1)
-  {
-    perror(device);
-    return;// 1;
-  }
-
-  srand(time(0));
-
-  int i = 0;
-  while (i < 4) {
-    sleep(1);
-    float position = randomFloat();
-    setServoPosition(fd, 0, position);
-    i++;
-  }
-   
-  close(fd);
-  //return 0;
+    return 0;
 }
--- platform/src/maestro/maestro.h
+++ platform/src/maestro/maestro.h
@@ -10,6 +10,6 @@ typedef struct servo_controller servo_controller;
 
 servo_controller *servo_controller_alloc(const char *device);
 void servo_controller_dealloc(servo_controller *controller);
-void servo_controller_set_position(servo_controller *controller, uint8_t channel, float value);
+int servo_controller_set_position(servo_controller *controller, uint8_t channel, float value);
 
 #endif
